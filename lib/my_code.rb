536835d27c26e75475caf65305f405dbdb7a03e1
Andrew Matheny <androidmatheny@gmail.com>
2019-10-21 01:37:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,17 @@ while i < array.length
 new_array
 end
 
+def reduce(array, start_point = nil)
+  if start_point
+    total = start_point
+    i = 0 
+  else 
+    total = array[0]
+    i = 1 
+  end
+  while i < array.length 
+  total = yield(total, array[i])
+  i += 1 
+  end
+total
+end 
