c33bd3892202f5cb4a8d85959c315931753fa6d7
Andrew Matheny <androidmatheny@gmail.com>
2019-10-21 01:31:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++
@@ -1,9 +0,0 @@
-def my_own_map(array)
-  new_array = Array.new 
-  counter = 0 
-  while counter < array.length do 
-    new_array.push(yield(array[counter]))
-    counter += 1 
-  end 
-  new_array
-end 
